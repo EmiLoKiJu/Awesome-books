80f0b714a71bb29c97acd6cb41c18c5846833adf
unknown <gabrielrozas1996@gmail.com>
2023-04-24 19:24:45 -0800

    Add functionalities for javascript

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-plusplus */
 
 // Checking if local storage is available
+
+let isStorage = false;
 function storageAvailable(type) {
   let storage;
   try {
@@ -29,18 +31,50 @@ function storageAvailable(type) {
 }
 
 if (storageAvailable('localStorage')) {
-  // console.log('local storage available');
-} else {
-  // console.log('Not local storage for us');
+  isStorage = true
 }
 
-function setInStorage() {
-  localStorage.setItem('Number fifty five', 55);
+// Books functionalities
+
+const bookscontainer = document.querySelector('.bookscontainer')
+const addbook = document.querySelector('.addbutton');
+const book = {
+  'title': '',
+  'author': '',
+};
+let collection = [];
+let strcoll = [];
+
+function appendNewBook(book) {
+  collection.push(book);
+  localStorage.setItem('bookcollection', JSON.stringify(collection));
+  const div1 = document.createElement('div');
+  let content = `<p>${book.title}</p><p>${book.author}</p><button class="removebutton">Remove</button><hr size="1">`;
+  div1.innerHTML = content;
+  bookscontainer.appendChild(div1);
+  removebutton = div1.querySelector('.removebutton');
+  removebutton.addEventListener('click', () => {
+    const index = collection.indexOf(book);
+    collection.splice(index, 1);
+    localStorage.setItem('bookcollection', JSON.stringify(collection));
+    div1.remove();
+  });
 }
 
-// function seeInStorage() {
-//   // console.log(localStorage.getItem('Number fifty five'));
-// }
+addbook.addEventListener('click', () => {
+  const newbook = Object.create(book);
+  newbook.title = document.querySelector('#title').value;
+  newbook.author = document.querySelector('#author').value;
+  appendNewBook(newbook);
+});
 
-setInStorage();
-// seeInStorage();
+document.addEventListener('DOMContentLoaded', () => {
+  let booksstoraged = JSON.parse(localStorage.getItem('bookcollection'));
+  if (isStorage && booksstoraged != null) {
+    for (let i = 0; i < booksstoraged.length; i++) {
+      appendNewBook(booksstoraged[i]);
+    }
+  } else  {
+    console.log('empty');
+  }
+});
